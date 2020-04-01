e0579bfa3f98176f7894386bd310b38844b6794b
LiuYang <1005859278@qq.com>
2020-04-01 18:18:18 +0800

    Mybatis实操

--- src/main/java/com/company/project/dao/UserInfoMapper.java
+++ src/main/java/com/company/project/dao/UserInfoMapper.java
@@ -3,5 +3,9 @@ package com.company.project.dao;
 import com.company.project.core.Mapper;
 import com.company.project.model.UserInfo;
 
+import org.apache.ibatis.annotations.Select;
+
 public interface UserInfoMapper extends Mapper<UserInfo> {
+    @Select("select * from user_info")
+    UserInfo findPerson();
 }
