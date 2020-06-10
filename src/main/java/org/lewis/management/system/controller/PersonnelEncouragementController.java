9869afb4d4f9f8059fbbb1766696cdb3681db8a0
LiuYang <1005859278@qq.com>
2020-06-10 09:18:50 +0800

    人事管理系统-人事管理-新增基本增删改查接口

--- src/main/java/org/lewis/management/system/controller/PersonnelEncouragementController.java
+++ src/main/java/org/lewis/management/system/controller/PersonnelEncouragementController.java
@@ -1,18 +1,57 @@
 package org.lewis.management.system.controller;
+import org.lewis.management.system.core.Result;
+import org.lewis.management.system.core.ResultGenerator;
+import org.lewis.management.system.dto.request.PersonnelInfoReqDTO;
+import org.lewis.management.system.dto.response.PersonnelInfoResDTO;
 import org.lewis.management.system.service.PersonnelEncouragementService;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
 /**
-* Created by Lewis on 2020/06/09.
-*/
+ * 员工奖惩
+ *
+ * @author lewis[1005859278@qq.com]
+ * @date 2020/06/09
+ * @since 1.0
+ */
 @RestController
 @RequestMapping("/personnel/encouragement")
+@Api(tags = "员工奖惩")
 public class PersonnelEncouragementController {
     @Resource
     private PersonnelEncouragementService personnelEncouragementService;
 
+    @PostMapping("/add")
+    @ApiOperation("新增员工基本信息")
+    public Result add(@RequestBody PersonnelInfoReqDTO req) {
+        return ResultGenerator.genSuccessResult();
+    }
 
+    @GetMapping("/list")
+    @ApiOperation("查询员工基本信息")
+    public PersonnelInfoResDTO list() {
+        return null;
+    }
+
+
+    @DeleteMapping("/delete")
+    @ApiOperation("删除员工基本信息")
+    public Result delete() {
+        return ResultGenerator.genSuccessResult();
+    }
+
+    @PostMapping("/update")
+    @ApiOperation("更新员工基本信息")
+    public Result update() {
+        return ResultGenerator.genSuccessResult();
+    }
 }
--- src/main/java/org/lewis/management/system/controller/SalaryController.java
+++ src/main/java/org/lewis/management/system/controller/SalaryController.java
@@ -1,17 +1,58 @@
 package org.lewis.management.system.controller;
+import org.lewis.management.system.core.Result;
+import org.lewis.management.system.core.ResultGenerator;
+import org.lewis.management.system.dto.request.PersonnelInfoReqDTO;
+import org.lewis.management.system.dto.response.PersonnelInfoResDTO;
 import org.lewis.management.system.service.SalaryService;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
 /**
-* Created by Lewis on 2020/06/09.
-*/
+ * 薪资
+ *
+ * @author lewis[1005859278@qq.com]
+ * @date 2020/06/09
+ * @since 1.0
+ */
 @RestController
 @RequestMapping("/salary")
+@Api(tags = "薪资")
 public class SalaryController {
     @Resource
     private SalaryService salaryService;
+
+    @PostMapping("/add")
+    @ApiOperation("新增员工基本信息")
+    public Result add(@RequestBody PersonnelInfoReqDTO req) {
+        return ResultGenerator.genSuccessResult();
+    }
+
+    @GetMapping("/list")
+    @ApiOperation("查询员工基本信息")
+    public PersonnelInfoResDTO list() {
+        return null;
+    }
+
+
+    @DeleteMapping("/delete")
+    @ApiOperation("删除员工基本信息")
+    public Result delete() {
+        return ResultGenerator.genSuccessResult();
+    }
+
+    @PostMapping("/update")
+    @ApiOperation("更新员工基本信息")
+    public Result update() {
+        return ResultGenerator.genSuccessResult();
+    }
 
 }
