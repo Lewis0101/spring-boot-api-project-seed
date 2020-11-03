79729e0ca280dbbf33b4efaff1753333f757d60a
LiuYang <1005859278@qq.com>
2020-11-03 16:07:02 +0800

    人事管理系统-Cookie工具类

---
+++ src/main/java/org/lewis/management/system/common/utils/CookieUtil.java
@@ -0,0 +1,67 @@
+package org.lewis.management.system.common.utils;
+
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @author : LiuYang[1005859278@qq.com]
+ * @date : 2020/11/3
+ * @decs: Cookie工具类，提供Cookie的创建、获取、删除方法
+ */
+@Component
+public class CookieUtil {
+
+    public CookieUtil() {
+    }
+
+    /**
+     * 创建Cookie
+     *
+     * @param response
+     * @param name
+     * @param value
+     * @param maxAge
+     */
+    public static void addCookie(HttpServletResponse response, String name, String value, int maxAge) {
+        Cookie cookie = new Cookie(name, value);
+        cookie.setPath("/");
+        if (maxAge > 0) {
+            cookie.setMaxAge(maxAge);
+        }
+        response.addCookie(cookie);
+    }
+
+    /**
+     * 删除Cookie
+     *
+     * @param response
+     * @param name
+     */
+    public static void removeCookie(HttpServletResponse response, String name) {
+        Cookie cookie = new Cookie(name, null);
+        cookie.setPath("/");
+        cookie.setMaxAge(0);
+        response.addCookie(cookie);
+    }
+
+    /**
+     * 获取cookie值
+     *
+     * @param request
+     * @param cookieName
+     * @return
+     */
+    public static String getUid(HttpServletRequest request, String cookieName) {
+        Cookie[] cookies = request.getCookies();
+        for (Cookie cookie : cookies) {
+            if (cookie.getName().equals(cookieName)) {
+                return cookie.getValue();
+            }
+        }
+        return null;
+    }
+
+}
--- src/main/java/org/lewis/management/system/controller/LoginController.java
+++ src/main/java/org/lewis/management/system/controller/LoginController.java
@@ -1,5 +1,6 @@
 package org.lewis.management.system.controller;
 
+import org.lewis.management.system.common.utils.CookieUtil;
 import org.lewis.management.system.dto.request.UserReqDTO;
 import org.lewis.management.system.dto.response.UserResDTO;
 import org.lewis.management.system.service.UserService;
@@ -10,25 +11,33 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Objects;
+
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * Created by Lewis on 2020/11/02.
+ *
+ * @author :lys
  */
 @RestController
 @RequestMapping("/user")
 @Api(tags = "用户登录")
+@Slf4j
 public class LoginController {
     @Resource
     private UserService userService;
 
     @GetMapping
     @ApiOperation(notes = "查询用户信息", value = "查询用户信息")
-    public UserResDTO getUserInfo(@RequestParam("user_name") String username) {
+    public UserResDTO getUserInfo(HttpServletResponse response, @RequestParam("user_name") String username) {
+        CookieUtil.addCookie(response, username, "123", 30);
         return userService.getUserInfo(username);
     }
 
@@ -36,6 +45,33 @@ public class LoginController {
     public String register(@Valid @RequestBody UserReqDTO req) {
         userService.insertUser(req);
         return "register success";
+    }
+
+    @PostMapping("/login")
+    @ApiOperation(notes = "查询用户信息", value = "查询用户信息")
+    public String login(HttpServletResponse response, @Valid @RequestBody UserReqDTO req) {
+
+        if ("".equals(req.getName()) || "".equals(req.getPassword())){
+            try {
+                throw new Exception("用户名或者密码为空");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        UserResDTO userInfo = userService.getUserInfo(req.getName());
+        if (Objects.isNull(userInfo)){
+            log.info("当前人员未注册！请注册！");
+            try {
+                throw new  Exception("当前人员未注册！请注册！");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        CookieUtil.addCookie(response,req.getName(),req.getPassword(),30);
+
+        return "登陆成功";
     }
 
 }
